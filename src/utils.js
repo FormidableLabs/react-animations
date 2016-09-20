54ddc8dfd5be12186f5b6ebfb6f2a9f043d43c8d
Brandon Dail <cottoncrypt@gmail.com>
2016-09-19 20:24:04 -0500

    Make compose return a function
    Instead of composing strings, we can just create a new function that
    returns the composed string.

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,18 @@
 // @flow
+import type { CSSValue } from './types';
 
 
-export const compose = (...args: Array<string>): string => {
-  const result: string = args.reduce(
-    (acc, value) => `${acc} ${value}`
-  );
-  return result.trim();
+export const compose = (...funcs: Array<Function>) => {
+  return (...styleArgs) => {
+    const result = funcs.reduce(
+      (acc, func, i) => `${acc} ${func(...styleArgs[i])}`,
+      ''
+    );
+    return result.trim();
+  }
 }
+
+
 export const cubicBezier = (
   a: number,
   b: number,
@@ -14,20 +20,30 @@ export const cubicBezier = (
   d: number
 ): string => `cubic-bezier(${a}, ${b}, ${c}, ${d})`;
 
-export const translate3d = (a: number, b: number, c: number): string =>
-  `translate3d(${a}px, ${b}px, ${c}px)`;
+export const translate3d = (
+  a: CSSValue,
+  b: CSSValue,
+  c: CSSValue
+): string => `translate3d(${a}, ${b}, ${c})`
 
-export const scale3d = (a: number, b: number, c: number): string =>
-  `scale3d(${a}, ${b}, ${c})`;
+export const scale3d = (
+  a: number,
+  b: number,
+  c: number
+): string => `scale3d(${a}, ${b}, ${c})`;
 
-export const skewX = (deg: number): string =>
-  `skewX(${deg}deg)`;
+export const skewX = (
+  deg: number
+): string => `skewX(${deg}deg)`;
 
-export const skewY = (deg: number): string =>
-  `skewY(${deg}deg)`;
+export const skewY = (
+  deg: number
+): string => `skewY(${deg}deg)`;
 
-export const skewXY = (x: number, y: number): string =>
-  `${skewX(x)} ${skewY(y)}`;
+export const skewXY = (
+  x: number,
+  y: number
+): string => `${skewX(x)} ${skewY(y)}`;
 
 export const rotate3d = (
   a: number,
