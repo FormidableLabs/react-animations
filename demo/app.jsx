246ff684a8e17459bef5f8a7ad364907960c0a83
Brandon Dail <cottoncrypt@gmail.com>
2016-09-21 13:44:00 -0500

    Add Aphrodite to demo
    The demo can now be run on Aphrodite or Radium

--- demo/app.jsx
+++ demo/app.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Radium, { Style, StyleRoot } from "radium";
+import { StyleSheet, css } from 'aphrodite';
 import * as animations from '../lib';
 
 
@@ -19,6 +20,22 @@ const styles = {
     backfaceVisibilty: 'visible',
   },
 };
+
+let aphroditeStyle = {
+  global: {
+    textAlign: 'center',
+    fontFamily: 'sans-serif',
+    paddingTop: 200,
+  },
+  swing: {
+    transformOrigin: 'top center'
+  },
+  flip: {
+    backfaceVisibilty: 'visible',
+  },
+}
+
+
 const animationNames = [];
 
 for (let key in animations) {
@@ -32,10 +49,19 @@ for (let key in animations) {
     animation: 'x 1s ease',
     animationName: Radium.keyframes(animation, key),
   };
+  aphroditeStyle[key] = {
+    ...aphroditeStyle[key],
+    animationName: animation,
+    animationDuration: '1s',
+    animationTimingFunction: 'ease',
+  };
 }
 
-let AnimationDemo = ({ style, animation, selectAnimation }) => (
+aphroditeStyle = StyleSheet.create(aphroditeStyle)
+
+let AnimationRadiumDemo = ({ style, animation, selectAnimation }) => (
   <div style={styles.global}>
+    <span>Using Radium</span>
     <h1 style={style}>Hello, world</h1>
     <select value={animation} onChange={selectAnimation}>
     {animationNames.map(name => (
@@ -45,7 +71,20 @@ let AnimationDemo = ({ style, animation, selectAnimation }) => (
   </div>
 )
 
-AnimationDemo = Radium(AnimationDemo);
+AnimationRadiumDemo = Radium(AnimationRadiumDemo);
+
+
+let AnimationAphroditeDemo = ({ style, animation, selectAnimation }) => (
+  <div className={css(style.global)}>
+    <span>Using Aphrodite</span>
+    <h1 className={css(style[animation])}>Hello, world</h1>
+    <select value={animation} onChange={selectAnimation}>
+    {animationNames.map(name => (
+      <option key={name} value={name}>{name}</option>
+    ))}
+  </select>
+  </div>
+)
 
 class App extends React.Component {
 
@@ -53,6 +92,7 @@ class App extends React.Component {
     super();
     this.state = {
       animation: 'bounce',
+      library: 'Radium',
     }
     this.selectAnimation = this.selectAnimation.bind(this);
   }
@@ -62,16 +102,48 @@ class App extends React.Component {
     this.setState({ animation: event.target.value })
   }
 
+  onLibraryChange(library) {
+    console.log(library)
+    this.setState({ library })
+  }
+
   render() {
-    const { animation } = this.state;
+    const { animation, library } = this.state;
     const style = styles[animation];
+    console.log(animation, aphroditeStyle);
+    let demo;
+    if (library === 'Aphrodite') {
+      demo = (
+        <div>
+          <AnimationAphroditeDemo
+            selectAnimation={this.selectAnimation}
+            animation={animation}
+            style={aphroditeStyle}
+          />
+        </div>
+      )
+    } else {
+      demo = (
+        <StyleRoot>
+          <AnimationRadiumDemo
+            selectAnimation={this.selectAnimation}
+            animation={animation}
+            style={style} />
+        </StyleRoot>
+      )
+    }
     return (
-      <StyleRoot>
-        <AnimationDemo
-          selectAnimation={this.selectAnimation}
-          animation={animation}
-          style={style} />
-      </StyleRoot>
+      <div>
+        {demo}
+        <button
+          onClick={this.onLibraryChange.bind(this, 'Radium')}>
+            Use Radium
+        </button>
+        <button
+          onClick={this.onLibraryChange.bind(this, 'Aphrodite')}>
+            Use Aphrodite
+        </button>
+      </div>
     )
   }
 }
