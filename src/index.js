5957f9c774168bf8ffa072c78d08468581966500
Brandon Dail <cottoncrypt@gmail.com>
2016-09-21 13:16:42 -0500

    Add rotateOutUpRight animation

--- src/index.js
+++ src/index.js
@@ -69,3 +69,4 @@ export rotateOut from './components/rotateOut.js';
 export rotateOutDownLeft from './components/rotateOutDownLeft.js';
 export rotateOutDownRight from './components/rotateOutDownRight.js';
 export rotateOutUpLeft from './components/rotateOutUpLeft.js';
+export rotateOutUpRight from './components/rotateOutUpRight.js';
